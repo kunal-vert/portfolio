af4b286ca8f0c8529522c044737c07803271b165
kunal <kunalk8728@gmail.com>
2026-06-17 21:08:23 +0530

    day-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+   
+      <div className='bg-black absolute opacity-75 h-screen w-screen text-white'>
+        <div>header</div>
+        <div>main</div>
+        <div>footer</div>
+
+      </div>
+    
   )
 }
 
