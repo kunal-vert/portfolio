4bd479a4ebe0dce5dd4fcbb4d61690683eb0269a
kunal <kunalk8728@gmail.com>
2026-07-21 20:51:42 +0530

    adding the education

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div>
+        school
+      </div>
+      <div>
+        university
+      </div>
+    </div>
   )
 }
 
